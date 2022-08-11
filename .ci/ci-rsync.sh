29cdaf9a5135abe60bba7a359e9a0bd7df39bede
Sylwester Nakonieczny <snakonieczny@gmail.com>
2022-08-11 06:38:47 +0200

    ci: fix uploading to server

--- .ci/ci-rsync.sh
+++ .ci/ci-rsync.sh
@@ -2,16 +2,23 @@
 
 pacman -S --needed --noconfirm --noprogressbar openssh rsync
 source <(ssh-agent)
-ssh-add <(cat <<<${SSH_KEY})
+mkdir -p ~/.ssh
+echo "Host *" > ~/.ssh/config     
+echo " StrictHostKeyChecking no" >> ~/.ssh/config
+cat <<<${SSH_KEY} > ~/.ssh/id_ed25515
 
 pushd msys2-artifacts
 echo "::group::[rsync] msys2-artifacts"
-rsync -av ${SS_USER}@${SSH_HOST}:/srv/repo/x86_64/msys2/ .
+if [ "$(/usr/bin/ls -A .)" ]; then
+    rsync -av -e 'ssh -i ~/.ssh/id_ed25515' . ${SS_USER}@${SSH_HOST}:/srv/repo/x86_64/msys2/
+fi
 echo "::endgroup::"
 popd
 
 pushd posix-artifacts
 echo "::group::[rsync] posix-artifacts"
-rsync -av ${SS_USER}@${SSH_HOST}:/srv/repo/x86_64/posix/ .
+if [ "$(/usr/bin/ls -A .)" ]; then
+    rsync -av -e 'ssh -i ~/.ssh/id_ed25515' . ${SSH_USER}@${SSH_HOST}:/srv/repo/x86_64/posix/
+fi
 echo "::endgroup::"
 popd
